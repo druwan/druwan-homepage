fd7d74197d9515766fe833b09185588dec88516e
Christopher Vestman <christophervestman@gmail.com>
2022-05-31 14:58:45 +0200

    fix: less text-indentation

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -2,8 +2,8 @@ import { Box } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 export const BioSection = styled(Box)`
-  padding-left: 3.5em;
-  text-indent: -3.5em;
+  padding-left: 3em;
+  text-indent: -3em;
 `;
 
 export const BioYear = styled.span`
