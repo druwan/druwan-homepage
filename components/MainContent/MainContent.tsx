3f68b3b8678c37a2cf0921f8440a5241df26959c
Christopher Vestman <christophervestman@gmail.com>
2022-10-25 12:43:26 +0200

    feat: responsive projects
    closes #17

--- components/MainContent/MainContent.tsx
+++ components/MainContent/MainContent.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Container,
-  Heading,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 
 import Projects from './Projects';
 
@@ -12,27 +6,30 @@ const MainContent = () => {
   const textColor = useColorModeValue('snow.500', 'princetonOrange.500');
 
   return (
-    <Container maxW={'70%'}>
-      <Box my={'16px'}>
-        <Heading
-          as={'em'}
-          color={textColor}
-          fontSize={{ base: 'xl', md: '2xl', lg: '4xl' }}>
-          Engineer &amp; Developer
-        </Heading>
-        <Text my={4} fontSize={{ base: 'md' }}>
-          I am currently employed by Experis. I have a passion for math,
-          physics, web &amp; football. When not online, I like to{' '}
-          <Text as={'s'} color={textColor}>
-            eat,
-          </Text>{' '}
-          cook, play the guitar or listening to podcasts.
-        </Text>
-      </Box>
-      <Box my={'32px'}>
+    <Box maxW={'70%'}>
+      <Flex
+        my={'16px'}
+        justifyContent={'start'}
+        flexDir={'row'}
+        align={'center'}>
+        <Box>
+          <Heading as={'em'} fontSize={{ base: 'xl', md: '2xl', lg: '4xl' }}>
+            Engineer &amp; Developer
+          </Heading>
+          <Text my={4} fontSize={{ base: 'md' }}>
+            I am currently employed by Experis. I have a passion for math,
+            physics, web &amp; football. When not online, I like to{' '}
+            <Text as={'s'} color={textColor}>
+              eat,
+            </Text>{' '}
+            cook, play the guitar or listening to podcasts.
+          </Text>
+        </Box>
+      </Flex>
+      <Flex my={'32px'} justifyContent={'center'} maxW={'100%'}>
         <Projects />
-      </Box>
-    </Container>
+      </Flex>
+    </Box>
   );
 };
 
