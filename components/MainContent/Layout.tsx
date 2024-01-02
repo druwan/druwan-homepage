564713bddca7d19b2dcd27a36a39719b35510a3d
Christopher Vestman <christophervestman@gmail.com>
2024-01-02 21:48:19 +0100

    refactor: rm header

--- components/MainContent/Layout.tsx
+++ components/MainContent/Layout.tsx
@@ -2,14 +2,12 @@ import { Flex } from '@chakra-ui/react';
 
 import React, { ReactNode } from 'react';
 import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
 
 type LayoutProps = { children?: ReactNode };
 
 const Layout = (props: LayoutProps) => {
   return (
     <>
-      <Header />
       <Flex as={'main'} justifyContent={'center'}>
         {props.children}
       </Flex>
