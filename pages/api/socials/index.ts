6ea62179f04d22408e4210d253dae1d58be14917
Christopher Vestman <christophervestman@gmail.com>
2024-04-01 17:50:06 +0200

    refactor: removed some socials

--- pages/api/socials/index.ts
+++ pages/api/socials/index.ts
@@ -10,14 +10,14 @@ const socials: ISocials[] = [
     name: 'LinkedIn',
     link: 'https://www.linkedin.com/in/christophervestman/',
   },
-  {
-    name: 'Instagram',
-    link: 'https://www.instagram.com/christophervestman/',
-  },
-  {
-    name: 'Twitch',
-    link: 'https://www.twitch.tv/druwan',
-  },
+  // {
+  //   name: 'Instagram',
+  //   link: 'https://www.instagram.com/christophervestman/',
+  // },
+  // {
+  //   name: 'Twitch',
+  //   link: 'https://www.twitch.tv/druwan',
+  // },
 ];
 
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
