08d1289e175cb2475323e6a6cc89bd68eda90cf4
Christopher Vestman <christophervestman@gmail.com>
2024-06-29 10:51:20 +0200

    #36 - angular icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,6 +1,13 @@
 import { SVGProps } from 'react';
 
 export const CustomIcons = {
+  angular: (props: SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fill="currentColor"
+        d="M185.7 268.1h76.2l-38.1-91.6-38.1 91.6zM223.8 32L16 106.4l31.8 275.7 176 97.9 176-97.9 31.8-275.7zM354 373.8h-48.6l-26.2-65.4H168.6l-26.2 65.4H93.7L223.8 81.5z"></path>
+    </svg>
+  ),
   email: (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g></g>
