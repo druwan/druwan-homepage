c2c25b020bd739a3b6e9907bc6ddfedb025858b5
Christopher Vestman <christophervestman@gmail.com>
2024-06-30 20:19:46 +0200

    #36 - Cypress icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -29,6 +29,15 @@ export const CustomIcons = {
         d="M12 0C5.352 0 0 5.352 0 12s5.352 12 12 12 12-5.352 12-12S18.648 0 12 0zm2.8 4.333c.13-.004.248.136.171.278l-3.044 5.58a.187.187 0 00.164.276h5.26c.17 0 .252.207.128.323l-9.22 8.605c-.165.154-.41-.063-.278-.246l4.364-6.021a.187.187 0 00-.151-.296H6.627a.187.187 0 01-.131-.32l8.18-8.123a.182.182 0 01.125-.056z"></path>
     </svg>
   ),
+  cypress: (props: SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fill="currentColor"
+        d="M19.48 17.007a9 9 0 1 0 -7.48 3.993c.896 0 1.691 -.573 1.974 -1.423l3.526 -10.577"></path>
+      <path fill="currentColor" d="M13.5 9l2 6"></path>
+      <path fill="currentColor" d="M10.764 9.411a3 3 0 1 0 -.023 5.19"></path>
+    </svg>
+  ),
   email: (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g></g>
