92cc85d98e94ecdebe8fa83178db865ec35e2888
Christopher Vestman <christophervestman@gmail.com>
2025-01-06 15:04:04 +0100

    refactor: error block

--- src/app/components/ProjectsCarousel.tsx
+++ src/app/components/ProjectsCarousel.tsx
@@ -17,12 +17,21 @@ type Project = {
 
 export function ProjectCarousel() {
   const [projects, setProjects] = useState<Project[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProjects = async () => {
-      const response = await fetch('/api/projects');
-      const { data, error } = await response.json();
-      setProjects(data);
+      try {
+        const response = await fetch('/api/projects');
+        const { data, error } = await response.json();
+        if (error) {
+          setError(error);
+        } else {
+          setProjects(data);
+        }
+      } catch (err) {
+        setError(`Failed to fetch projects: ${err.message}`);
+      }
     };
     fetchProjects();
   }, []);
@@ -30,38 +39,39 @@ export function ProjectCarousel() {
   return (
     <Carousel plugins={[Autoplay({ delay: 3500, stopOnMouseEnter: false })]}>
       <CarouselContent>
-        {projects.map((project) => (
-          <CarouselItem key={project.id}>
-            <div className='flex aspect-video items-center justify-center'>
-              <Card>
-                <CardHeader className='text-2xl font-semibold tracking-tighter pb-2'>
-                  {project.title}
-                </CardHeader>
-                <div
-                  style={{
-                    position: 'relative',
-                    width: '100%',
-                    paddingTop: '300px',
-                    borderRadius: '15px',
-                    overflow: 'hidden',
-                  }}
-                >
-                  <Image
-                    alt={`Image of ${project.title}`}
-                    src={project.image_url}
-                    fill
-                    priority
-                    sizes='(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw'
-                    style={{ objectFit: 'cover' }}
-                  />
-                </div>
-                <CardContent className='justify-normal'>
-                  {project.description}
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
+        {!error &&
+          projects.map((project) => (
+            <CarouselItem key={project.id}>
+              <div className='flex aspect-video items-center justify-center'>
+                <Card>
+                  <CardHeader className='text-2xl font-semibold tracking-tighter pb-2'>
+                    {project.title}
+                  </CardHeader>
+                  <div
+                    style={{
+                      position: 'relative',
+                      width: '100%',
+                      paddingTop: '300px',
+                      borderRadius: '15px',
+                      overflow: 'hidden',
+                    }}
+                  >
+                    <Image
+                      alt={`Image of ${project.title}`}
+                      src={project.image_url}
+                      fill
+                      priority
+                      sizes='(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw'
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                  <CardContent className='justify-normal'>
+                    {project.description}
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
       </CarouselContent>
     </Carousel>
   );
