7facfadf3d2daf647198e2fc3d3ebc8ef3e3cee8
Christopher Vestman <christophervestman@gmail.com>
2025-01-06 12:30:55 +0100

    fix: sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,20 @@
+import { MetadataRoute } from 'next';
+
 export const baseUrl = 'https://christophervestman.dev';
 
-export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
-
-  let routes = ['', '/blog'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }));
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/blog/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
 }
