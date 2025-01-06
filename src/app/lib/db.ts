8fd613130ef1243ffb75b37cc24c588b1f430421
Christopher Vestman <christophervestman@gmail.com>
2025-01-06 13:16:15 +0100

    fix: move db to supabase

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -2,8 +2,8 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-  host: 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT) || 5431,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
   user: process.env.POSTGRES_USER,
   password: `${process.env.POSTGRES_PASSWORD}`,
   database: process.env.POSTGRES_DBNAME,
