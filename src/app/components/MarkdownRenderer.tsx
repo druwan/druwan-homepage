7f4018bb61ad2c3728f96cd448614ec0c5ee9ee1
Christopher Vestman <christophervestman@gmail.com>
2026-02-27 18:04:27 +0100

    fix(blog): md rendered properly.

--- src/app/components/MarkdownRenderer.tsx
+++ src/app/components/MarkdownRenderer.tsx
@@ -1,10 +1,11 @@
 'use client';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
     <div className='w-full max-w-full prose dark:prose-invert'>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
     </div>
   );
 }
